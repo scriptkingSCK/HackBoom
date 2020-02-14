a478aae8d6561c15db99763b56296c0284d8457d
scriptkingSCK <60373400+scriptkingSCK@users.noreply.github.com>
2020-02-14 08:51:31 +0700

    Create set.sh

---
+++ set.sh
@@ -0,0 +1,8 @@
+update
+pip install requirements.txt
+pip install requests
+pkg install python 3 -y
+pkg install wget -y
+echo -e "\e[1;89m"
+echo "   ติดตั้งเสร็จแล้วพิมคำสั่งเปิด bash HackBoom.sh"
+echo -e "\e[1;89m"
